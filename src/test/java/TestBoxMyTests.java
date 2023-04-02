3b7505f7f9e2558de345499eb9829fd71ef06c0b
juribt1718 <juribt@rambler.ru>
2023-04-03 00:57:23 +0300

    Новые правки

--- src/test/java/TestBoxMyTests.java
+++ src/test/java/TestBoxMyTests.java
@@ -10,8 +10,6 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestBoxMyTests {
-
-
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
@@ -23,42 +21,33 @@ public class TestBoxMyTests {
     void fillFormTest() {
         String userName = "Yuri";
         open("/automation-practice-form");
-        open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
-
+//Обращение к локаторам
         $("#firstName").setValue("Yuri");
         $("#lastName").setValue("Bilov");
-
-
         $("#userEmail").setValue("jurib@mail.com");
         $("#genterWrapper").$(byText("Other")).click();
         $("#userNumber").setValue("5678456666");
-
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("September");
         $(".react-datepicker__year-select").selectOption("1974");
         $(".react-datepicker__day--004:not(.react-datepicker__day--outside-month)").click();
         $("#subjectsInput").setValue("Biology").pressEnter();
         $("#hobbiesWrapper").$(byText("Reading")).click();
-
         $("#uploadPicture").uploadFile(new File("src/test/resources/img/Nebo.jpg"));
-
         $("#currentAddress").setValue("SomeTown");
-
-
         $("#state").click();
         $("#stateCity-wrapper").$(byText("Haryana")).click();
-
 //
         $("#submit").click();
-
+//Проверка введённых данных
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-
-
-        $(".table-responsive").shouldHave(text(userName),text("Yuri"), text("Bilov"), text("jurib@mail.com"));
-
+        $(".table-responsive").shouldHave(text("Yuri Bilov"), text("jurib@mail.com"), text("Other")
+                , text("5678456666"), text("04 September,1974"), text("Biology"), text("Reading")
+                , text("Nebo.jpg"), text("SomeTown"));
+//Закрываем окно
         $("#closeLargeModal").click();
     }
 
