f3744b71f4bae5bdbf2b362fcdfcb97a2d2e48f7
Bilov Yuriy <jurib@rambler.ru>
2023-05-14 00:08:07 +0300

    Page objects 1

---
+++ src/test/java/TestPageLaunchTests.java
@@ -0,0 +1,51 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+public class TestPageLaunchTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        System.setProperty("chromeoptions.args", "--remote-allow-origins=*");
+    }
+
+    @Test
+    void fillFormTest() {
+       new RegistrationPage().openPage();
+//Обращение к локаторам
+        $("#firstName").setValue("Yuri");
+        $("#lastName").setValue("Bilov");
+        $("#userEmail").setValue("jurib@mail.com");
+        $("#genterWrapper").$(byText("Other")).click();
+        $("#userNumber").setValue("5678456666");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("September");
+        $(".react-datepicker__year-select").selectOption("1974");
+        $(".react-datepicker__day--004:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Biology").pressEnter();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/img/Nebo.jpg"));
+        $("#currentAddress").setValue("SomeTown");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
+//
+        $("#submit").click();
+//Проверка введённых данных
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Yuri Bilov"), text("jurib@mail.com"), text("Other")
+                , text("5678456666"), text("04 September,1974"), text("Biology"), text("Reading")
+                , text("Nebo.jpg"), text("SomeTown"));
+//Закрываем окно
+        $("#closeLargeModal").click();
+    }
+
+}
---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class RegistrationPage {
+
+    public void openPage(){
+
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+    }
+
+}
